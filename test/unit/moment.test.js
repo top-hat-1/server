fa743c6b2223095dc8a4d477b65432295389ec62
Jack Lomax <lomax15@me.com>
2018-04-28 12:55:34 -0700

    projectId and category required test passes

--- test/unit/moment.test.js
+++ test/unit/moment.test.js
@@ -15,4 +15,11 @@ describe('moment model', () => {
         moment1._id = moment._id;
         assert.deepEqual(moment.toJSON(), moment1);
     });
+
+    it('projectId and category is required', () => {
+        const moment = new Moment({});
+        const { errors } = moment.validateSync();
+        assert.equal(errors.projectId.kind, 'required');
+        assert.equal(errors.category.kind, 'required');
+    });
 });
