19cc975d286ebc7e648b57a0960987fe3083f095
Charlie <heinercj@gmail.com>
2018-04-28 15:41:34 -0700

    modified project e2e to add owner property to project

--- test/e2e/project.test.js
+++ test/e2e/project.test.js
@@ -5,6 +5,8 @@ const { dropCollection } = require('./db');
 
 describe('project api', () => {
     before(() => dropCollection('projects'));
+    beforeEach(() => dropCollection('users'));
+
 
     let project1 = {
         projectName: 'Roof',
@@ -12,13 +14,28 @@ describe('project api', () => {
         owner: Types.ObjectId()
     };
 
+    let userData = {
+        name: 'Bill',
+        email: 'Bill@me.com',
+        password: 'abc'
+    }
+
+    before(() => {
+        return request
+            .post('/api/auth/signup')
+            .send(userData)
+            .then(({ body }) => userData = body);
+    });
+
     it('saves and gets project', () => {
+        project1.owner = userData._id;
         return request.post('/api/projects')
             .send(project1)
             .then(({ body }) => {
                 const { _id } = body;
                 assert.ok(_id);
                 assert.ok(body.coverPhotoId);
+                assert.equal(project1.owner, userData._id);
                 assert.equal(body.projectName, 'Roof');
                 project1 = body;
             });
