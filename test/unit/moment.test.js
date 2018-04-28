1e21c5ce617deaae4a7f74ce370b5179ba70f743
Jack Lomax <lomax15@me.com>
2018-04-28 13:00:35 -0700

    enum test on category passes

--- test/unit/moment.test.js
+++ test/unit/moment.test.js
@@ -3,13 +3,13 @@ const Moment = require('../../lib/models/Moment');
 const { Types } = require('mongoose');
 
 describe('moment model', () => {
+    let moment1 = {
+        projectId: Types.ObjectId(),
+        category: 'before',
+        caption: 'Cool garage',
+        comments: [Types.ObjectId()],
+    };
     it('moment is a valid model', () => {
-        const moment1 = {
-            projectId: Types.ObjectId(),
-            category: 'before',
-            caption: 'Cool garage',
-            comments: [Types.ObjectId()],
-        };
 
         const moment = new Moment(moment1);
         moment1._id = moment._id;
@@ -21,5 +21,12 @@ describe('moment model', () => {
         const { errors } = moment.validateSync();
         assert.equal(errors.projectId.kind, 'required');
         assert.equal(errors.category.kind, 'required');
+    });
+
+    it('category has to be enum', () => {
+        moment1.category = 'finished';
+        const moment = new Moment(moment1);
+        const { errors } = moment.validateSync();
+        assert.equal(errors.category.kind, 'enum');
     });
 });
