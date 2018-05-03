5957a84cc0d7ac0e88552c8a862621c47a43815c
Charlie <heinercj@gmail.com>
2018-05-03 10:27:44 -0700

    refactored get moments and comments routes for project

--- test/e2e/project.test.js
+++ test/e2e/project.test.js
@@ -129,7 +129,7 @@ describe('project api', () => {
             .then(() => {
                 return request.get(`/api/projects/${project1._id}/moments`)
                     .then(({ body }) => {
-                        assert.equal(body.moments[0].caption, 'Master Bath');
+                        assert.equal(body[0].caption, 'Master Bath');
                     }); 
             });
     });
@@ -141,7 +141,7 @@ describe('project api', () => {
             .then(() => {
                 return request.get(`/api/projects/${project1._id}/comments`)
                     .then(({ body }) => {
-                        assert.equal(body.comments[0].comment, 'Nice work');
+                        assert.equal(body[0].comment, 'Nice work');
                     });
             });
     });
